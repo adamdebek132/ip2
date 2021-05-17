07625f76ad3877efcf8daadc7bc5a30aa075131b
adamdebek132 <66127494+adamdebek132@users.noreply.github.com>
2021-05-17 15:38:32 +0200

    first commit

---
+++ Utils/CustomAsyncPageFilter.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ip2.Utils
+{
+    public class CustomAsyncPageFilter : IAsyncPageFilter
+    {
+        private readonly IConfiguration _config;
+        public string ClientIPAddr { get; private set; }
+
+        public CustomAsyncPageFilter(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public Task OnPageHandlerSelectionAsync(PageHandlerSelectedContext context)
+        {
+            ClientIPAddr = context.HttpContext.Connection.RemoteIpAddress.ToString();
+            
+            return Task.CompletedTask;
+        }
+
+        public async Task OnPageHandlerExecutionAsync(PageHandlerExecutingContext context,
+                                                      PageHandlerExecutionDelegate next)
+        {
+            var result = context.Result;
+            if (result is PageResult)
+            {
+                var page = ((PageResult)result);
+                page.ViewData["IP"] = "siema";
+            }
+
+            await next.Invoke();
+        }
+    }
+}
---
+++ Utils/CustomPageFilterAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ip2.Utils
+{
+    public class CustomPageFilterAttribute : ResultFilterAttribute
+    {
+        public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+        {
+            var ClientIPAddr = context.HttpContext.Connection.RemoteIpAddress.MapToIPv4();
+            var result = context.Result;
+            if (result is PageResult)
+            {
+                var page = ((PageResult)result);
+                page.ViewData["IP"] = ClientIPAddr.ToString();
+            }
+            await next.Invoke();
+        }
+    }
+}
